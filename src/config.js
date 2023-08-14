d6c1aa00b33acf25978ed9df7f93da1ad4260f82
saurabhraj123 <me4saurabh4u@gmail.com>
2023-08-15 01:15:18 +0530

    fix: deployment

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_URI = "https://protected-docs-server.onrender.com:10000";
+export const BACKEND_URI = "https://protected-docs-server.onrender.com";
 export const SOCKET_URI = "https://protected-docs-server.onrender.com:10001";
