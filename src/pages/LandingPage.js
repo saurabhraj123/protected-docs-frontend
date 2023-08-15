3f00337c09192d65c5e5624886c0f97c6b9c1f68
saurabhraj123 <me4saurabh4u@gmail.com>
2023-08-15 11:06:13 +0530

    feature: create landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import classes from "./LandingPage.module.css";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  const [roomName, setRoomName] = useState("");
+  const navigate = useNavigate();
+
+  return (
+    <div className={classes.page}>
+      <div className={classes.container}>
+        <h1 className={classes.heading}>Welcome to Protected Docs</h1>
+        <p className={classes.subText}>
+          Access your secure documents in real-time.
+        </p>
+        <div className={classes.form}>
+          <input
+            type="text"
+            name="roomName"
+            placeholder="Enter room name"
+            onChange={(e) => setRoomName(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            className={classes.submitBtn}
+            onClick={() => navigate(`/${roomName}`)}
+          >
+            Create / Access Room
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
