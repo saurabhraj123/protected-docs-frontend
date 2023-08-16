ea2b7905e49eb3b9f826b8bfe5a07d752d8b420a
saurabhraj123 <me4saurabh4u@gmail.com>
2023-08-17 00:08:28 +0530

    fix: logo takes to home page

--- src/components/HeaderMenu/HeaderMenu.js
+++ src/components/HeaderMenu/HeaderMenu.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import classes from "./HeaderMenu.module.css";
 
 const HeaderMenu = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={classes.container}>
       <div className={classes.headContainer}>
-        <img src="logo.png" alt="logo" className={classes.logo} />
+        <img
+          src="logo.png"
+          alt="logo"
+          className={classes.logo}
+          onClick={() => navigate("/")}
+        />
         <div className={classes.headBtncontainer}>
           <button className={classes.button}>Change Password</button>
           <button className={classes.button}>Delete</button>
