5decbf8d248e318a2509e333eae6d04fbde90931
saurabhraj123 <me4saurabh4u@gmail.com>
2023-08-17 15:02:03 +0530

    feature: implement room delete

--- src/components/HeaderMenu/HeaderMenu.js
+++ src/components/HeaderMenu/HeaderMenu.js
@@ -1,9 +1,33 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import classes from "./HeaderMenu.module.css";
+import axios from "axios";
+import { BACKEND_URI } from "../../config";
 
 const HeaderMenu = () => {
   const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete your account?"
+    );
+    if (confirm) {
+      try {
+        const token = sessionStorage.getItem("token");
+        const res = await axios.delete(`${BACKEND_URI}/api/rooms/delete`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (res.status === 200) {
+          navigate("/");
+        }
+      } catch (err) {
+        console.log("err", err);
+      }
+    }
+  };
 
   return (
     <div className={classes.container}>
@@ -16,7 +40,9 @@ const HeaderMenu = () => {
         />
         <div className={classes.headBtncontainer}>
           <button className={classes.button}>Change Password</button>
-          <button className={classes.button}>Delete</button>
+          <button className={classes.button} onClick={handleDelete}>
+            Delete
+          </button>
         </div>
       </div>
     </div>
